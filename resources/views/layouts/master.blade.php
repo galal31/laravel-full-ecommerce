d4c7691af74ba6d8938b860cffae56c45aee8e02
Galal Nasser <glglnasser3110@gmail.com>
2026-02-19 03:16:33 +0200

    admin theme updates

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="rtl">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description"
+        content="Modern admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities with bitcoin dashboard.">
+    <meta name="keywords"
+        content="admin template, modern admin template, dashboard template, flat admin template, responsive admin template, web app, crypto dashboard, bitcoin dashboard">
+    <meta name="author" content="PIXINVENT">
+    <title>@yield('title', 'Modern Admin')</title>
+    <link rel="apple-touch-icon" href="{{ asset('dashboard-assets/app-assets/images/ico/apple-icon-120.png') }}">
+    <link rel="shortcut icon" type="image/x-icon"
+        href="{{ asset('dashboard-assets/app-assets/images/ico/favicon.ico') }}">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Quicksand:300,400,500,700"
+        rel="stylesheet">
+    <link href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css" rel="stylesheet">
+    @if (config('app.locale') == 'ar')
+        <link rel="stylesheet" type="text/css" href="{{ asset('dashboard-assets/app-assets/css-rtl/vendors.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('dashboard-assets/app-assets/css-rtl/app.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('dashboard-assets/app-assets/css-rtl/custom-rtl.css') }}">
+        <link rel="stylesheet" type="text/css"
+            href="{{ asset('dashboard-assets/app-assets/css-rtl/core/menu/menu-types/vertical-overlay-menu.css') }}">
+        <link rel="stylesheet" type="text/css"
+            href="{{ asset('dashboard-assets/app-assets/css-rtl/core/colors/palette-gradient.css') }}">
+        <link rel="stylesheet" type="text/css"
+            href="{{ asset('dashboard-assets/app-assets/css-rtl/pages/timeline.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('dashboard-assets/app-assets/css-rtl/style.css') }}">
+    @else
+        <link rel="stylesheet" type="text/css" href="{{ asset('dashboard-assets/app-assets/css/vendors.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('dashboard-assets/app-assets/css/app.css') }}">
+        <link rel="stylesheet" type="text/css"
+            href="{{ asset('dashboard-assets/app-assets/css/core/menu/menu-types/vertical-overlay-menu.css') }}">
+        <link rel="stylesheet" type="text/css"
+            href="{{ asset('dashboard-assets/app-assets/css/core/colors/palette-gradient.css') }}">
+        <link rel="stylesheet" type="text/css"
+            href="{{ asset('dashboard-assets/app-assets/css/pages/timeline.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('dashboard-assets/app-assets/css/style.css') }}">
+    @endif
+</head>
+
+<body class="vertical-layout vertical-overlay-menu 2-columns menu-expanded fixed-navbar" data-open="click"
+    data-menu="vertical-overlay-menu" data-col="2-columns">
+
+    @include('layouts.partials.navbar')
+    @include('layouts.partials.sidebar')
+    <div class="app-content content">
+        <div class="content-wrapper">
+            @yield('content')
+        </div>
+    </div>
+
+    @include('layouts.partials.footer')
+    <script src="{{ asset('dashboard-assets/app-assets/vendors/js/vendors.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('dashboard-assets/app-assets/js/core/app-menu.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('dashboard-assets/app-assets/js/core/app.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('dashboard-assets/app-assets/js/scripts/customizer.js') }}" type="text/javascript"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use Mcamara\LaravelLocalization\LaravelLocalization as McamaraLaravelLocalization;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('changelog');
 });
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+    ],
+    function () {
+
+    }
+);
