94ddeed88a0e9ecbc017444915f9b0152a409657
Galal Nasser <glglnasser3110@gmail.com>
2026-02-22 07:21:40 +0200

    fix admins , added remember token

--- database/migrations/2026_02_19_040527_create_admins_table.php
+++ database/migrations/2026_02_19_040527_create_admins_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         $table->string('name');
         $table->string('email')->unique();
         $table->string('password');
-
+        $table->rememberToken();
         $table->foreignId('role_id')
               ->constrained('roles')
               ->onDelete('cascade');
